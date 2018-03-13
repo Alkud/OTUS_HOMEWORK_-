88eefcae4d406c6cbb48562a0038b4cfa6b700a8
Alkud <alexandr_kudinov@mail.ru>
2018-03-13 14:57:11 +0300

    Using tuple, non-copying filter

--- main.cpp
+++ main.cpp
@@ -3,40 +3,34 @@
 #include <iostream>
 #include <iterator>
 #include <string>
-#include <map>
-#include <set>
 #include <functional>
 #include <utility>
 #include <algorithm>
 #include <vector>
-#include <list>
+#include <tuple>
 #include <sstream>
 #include "ip_address_works.h"
 
 using stringVector = std::vector<std::string>;
-using ipVector = std::vector<uint32_t>;
-struct ipStruct
-{
-  uint32_t value{};
-  uint8_t byte3{};
-  uint8_t byte2{};
-  uint8_t byte1{};
-  uint8_t byte0{};
-};
 
-bool operator<(const ipStruct& a, const ipStruct& b)
+using ipTuple = std::tuple<std::uint32_t, uint8_t, uint8_t, uint8_t, uint8_t>;
+using ipTupleVector = std::vector<ipTuple>;
+
+uint32_t ipBytesToInteger(const uint8_t& d3, const uint8_t& d2,
+                          const uint8_t& d1, const uint8_t& d0)
 {
-  return a.value < b.value;
+  return (d3 >> 24) + (d2 >> 16) + (d1 >> 8) + d0;
 }
 
-bool operator>(const ipStruct& a, const ipStruct& b)
+bool operator > (const ipTuple& a, const ipTuple& b)
 {
-  return a.value > b.value;
+  return std::get<0>(a) > std::get<0>(b);
 }
 
-using ipStructVector = std::vector<ipStruct>;
-using ipStructList = std::list<ipStruct>;
-using ipStructMultiset = std::multiset<ipStruct, std::greater<>>;
+bool operator < (const ipTuple& a, const ipTuple& b)
+{
+  return std::get<0>(a) < std::get<0>(b);
+}
 
 stringVector split(std::string inputString, char delimiter)
 {
@@ -53,47 +47,49 @@ stringVector split(std::string inputString, char delimiter)
   return result;
 }
 
-template<class T>
-T filter(const T& inputVector, int byte3, int byte2 = -1, int byte1 = -1, int byte0 = -1)
+void filter(const ipTupleVector& inputVector, int byte3, int byte2 = -1, int byte1 = -1, int byte0 = -1)
 {
-  T result{};
-
-  for(auto addressStruct : inputVector)
+  for(auto addressTuple : inputVector)
   {
-    if (byte3 == addressStruct.byte3 &&
-        (byte2 == -1 || byte2 == addressStruct.byte2) &&
-        (byte1 == -1 || byte1 == addressStruct.byte1) &&
-        (byte0 == -1 || byte0 == addressStruct.byte0)   )
-      result.insert(addressStruct);
+    int d3 {std::get<1>(addressTuple)};
+    int d2 {std::get<2>(addressTuple)};
+    int d1 {std::get<3>(addressTuple)};
+    int d0 {std::get<4>(addressTuple)};
+    if ( byte3 ==  d3 &&
+        (byte2 == -1 || byte2 == d2) &&
+        (byte1 == -1 || byte1 == d1) &&
+        (byte0 == -1 || byte0 == d0)   )
+    {
+      std::cout << d3 << '.' << d2 << '.' << d1 << '.' << d0 << std::endl;
+    }
   }
-  return result;
 }
 
-template <uint8_t... args, class T>
-T filterAny(const T& inputVector)
+template <uint8_t... args>
+void filterAny(const ipTupleVector& inputVector)
 {
-  T result{};
   std::array<uint8_t, sizeof...(args)> argsArray{args...};
-  for(auto addressStruct : inputVector)
+  for(auto addressTuple : inputVector)
   {
+    int d3 {std::get<1>(addressTuple)};
+    int d2 {std::get<2>(addressTuple)};
+    int d1 {std::get<3>(addressTuple)};
+    int d0 {std::get<4>(addressTuple)};
     for (auto& byte : argsArray)
     {
-      if (byte == addressStruct.byte3 ||
-          byte == addressStruct.byte2 ||
-          byte == addressStruct.byte1 ||
-          byte == addressStruct.byte0   )
+      if (byte == d3 || byte == d2 ||
+          byte == d1 || byte == d0   )
       {
-        result.insert(addressStruct);
+        std::cout << d3 << '.' << d2 << '.' << d1 << '.' << d0 << std::endl;
         break;
       }
     }
   }
-  return result;
 }
 
 int main(int argc, char* argv[])
 {
-  ipStructMultiset addresses{};
+  ipTupleVector addresses{};
   try
   {
     std::string nextString;
@@ -105,54 +101,32 @@ int main(int argc, char* argv[])
       int byte3{}, byte2{}, byte1{}, byte0{};
       char ch{};
       stringStream >> byte3 >> ch >> byte2 >> ch >> byte1 >> ch >> byte0;
-      ipArray addressArray {static_cast<uint8_t>(byte0) ,static_cast<uint8_t>(byte1),
-            static_cast<uint8_t>(byte2), static_cast<uint8_t>(byte3)};
-      auto addressInteger {ipArrayToInteger(addressArray)};
-      ipStruct addressStruct{addressInteger, addressArray[3],
-            addressArray[2], addressArray[1], addressArray[0]};
-      addresses.insert(addressStruct);
+      uint32_t addressInteger {ipBytesToInteger( (uint8_t)byte0, (uint8_t)byte1,
+            (uint8_t)byte2, (uint8_t)byte3)};
+      addresses.push_back(std::tie(addressInteger, byte3, byte2, byte1, byte0));
     }
 
-    //std::sort(addresses.begin(), addresses.end(), std::greater<>());
-    //addresses.sort(std::greater<>());
+    /* Descending sorting */
+    std::sort(addresses.begin(), addresses.end(), std::greater<>());
 
     /* Output sorted values */
-    for (auto addressStruct : addresses)
+    for (auto addressTuple : addresses)
     {
-      std::cout << int(addressStruct.byte3) << "." <<
-                   int(addressStruct.byte2) << "." <<
-                   int(addressStruct.byte1) << "." <<
-                   int(addressStruct.byte0) << std::endl;
+      int d3 {std::get<1>(addressTuple)};
+      int d2 {std::get<2>(addressTuple)};
+      int d1 {std::get<3>(addressTuple)};
+      int d0 {std::get<4>(addressTuple)};
+      std::cout << d3 << '.' << d2 << '.' << d1 << '.' << d0 << std::endl;
     }
 
     /* Filter 1.*.*.*. */
-    auto filteredAddresses {filter(addresses, 1)};
-    for (auto addressStruct : filteredAddresses)
-    {
-      std::cout << int(addressStruct.byte3) << "." <<
-                    int(addressStruct.byte2) << "." <<
-                    int(addressStruct.byte1) << "." <<
-                    int(addressStruct.byte0) << std::endl;
-    }
+    filter(addresses, 1);
 
     /* Filter 46.70.*.* */
-    filteredAddresses = filter(addresses, 46, 70);
-    for (auto addressStruct : filteredAddresses)
-    {
-      std::cout << int(addressStruct.byte3) << "." <<
-                    int(addressStruct.byte2) << "." <<
-                    int(addressStruct.byte1) << "." <<
-                    int(addressStruct.byte0) << std::endl;
-    }
+    filter(addresses, 46, 70);
+
     /* Filter any 46 */
-    filteredAddresses = filterAny<46>(addresses);
-    for (auto addressStruct : filteredAddresses)
-    {
-      std::cout << int(addressStruct.byte3) << "." <<
-                    int(addressStruct.byte2) << "." <<
-                    int(addressStruct.byte1) << "." <<
-                    int(addressStruct.byte0) << std::endl;
-    }
+    filterAny<46>(addresses);
   }
   catch (const std::exception &e)
   {
